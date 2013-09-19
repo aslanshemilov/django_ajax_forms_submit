9e19e505f63f3e67de7b2ef33cb04619da75e425
Joshua Johanan <josh359@gmail.com>
2013-09-18 21:51:04 -0400

    another package is named django-ajax-forms

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 
 setup(
-    name='django-ajax-forms',
+    name='django-ajax-forms-submit',
     version='0.2.0',
     description='Static javascript to make any form ajaxable',
     keywords='django, javascript, ajax',
     author='Joshua Johanan',
-    url='https://github.com/johanan/django_ajax_forms',
+    url='https://github.com/johanan/django_ajax_forms_submit',
     license='MIT',
-    packages=['django_ajax_forms'],
+    packages=['django_ajax_forms_submit'],
     zip_safe=False,
     include_package_data=True
 )
